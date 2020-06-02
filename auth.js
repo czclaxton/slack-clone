1e0820cb098e368e5cb292fc3153ae6327cb11b7
Connor Claxton <czclaxton@gmail.com>
2020-06-02 15:51:23 -0500

    setup login with auth using tokens

---
+++ auth.js
@@ -0,0 +1,62 @@
+import jwt from "jsonwebtoken";
+import _ from "lodash";
+import bcrypt from "bcrypt";
+
+export const createTokens = async (user, secret, secret2) => {
+  const createToken = jwt.sign({ user: _.pick(user, ["id"]) }, secret, {
+    expiresIn: "1h",
+  });
+
+  const createRefreshToken = jwt.sign({ user: _.pick(user, "id") }, secret2, {
+    expiresIn: "7d",
+  });
+
+  return [createToken, createRefreshToken];
+};
+
+export const tryLogin = async (email, password, models, SECRET, SECRET2) => {
+  const user = await models.User.findOne({ where: { email }, raw: true });
+
+  if (!user) {
+    // cannot find user with given email
+    return {
+      ok: false,
+      errors: [
+        {
+          path: "email",
+          message: "Invalid Email",
+        },
+      ],
+    };
+  }
+
+  const valid = await bcrypt.compare(password, user.password);
+
+  if (!valid) {
+    // given password doesnt match stored password
+    return {
+      ok: false,
+      errors: [
+        {
+          path: "password",
+          message: "Invalid Password",
+        },
+      ],
+    };
+  }
+
+  // Token expires if user changes password
+  const refreshTokenSecret = user.password + SECRET2;
+
+  const [token, refreshToken] = await createTokens(
+    user,
+    SECRET,
+    refreshTokenSecret
+  );
+
+  return {
+    ok: true,
+    token,
+    refreshToken,
+  };
+};
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // Components
 import Home from './Home'
 import Register from './Register'
+import Login from './Login'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
       </Switch>
     </Router>
   )
