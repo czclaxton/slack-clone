135416bccb822e5379a1a2af8aa5ce9468afa062
Connor Claxton <czclaxton@gmail.com>
2020-06-03 18:02:35 -0500

    added afterValidate hook to handle hasing password

--- models/user.js
+++ models/user.js
@@ -1,31 +1,52 @@
+import bcrypt from "bcrypt";
+
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define("user", {
-    username: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        isAlphanumeric: {
-          args: true,
-          msg: "Username can only contain letters and numbers",
+  const User = sequelize.define(
+    "user",
+    {
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isAlphanumeric: {
+            args: true,
+            msg: "Username can only contain letters and numbers",
+          },
+          len: {
+            args: [3, 25],
+            msg: "Username must be between 3 and 25 characters in length",
+          },
         },
-        len: {
-          args: [3, 25],
-          msg: "Username must be between 3 and 25 characters in length",
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Invalid Email",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [8, 100],
+            msg: "Password must be at least 8 characters",
+          },
         },
       },
     },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        len: {
-          args: [8, 100],
-          msg: "Invalid Email",
+    {
+      hooks: {
+        afterValidate: async (user) => {
+          const hashedPassword = await bcrypt.hash(user.password, 12);
+          user.password = hashedPassword;
         },
       },
-    },
-    password: DataTypes.STRING,
-  });
+    }
+  );
 
   User.associate = (models) => {
     User.belongsToMany(models.Team, {
